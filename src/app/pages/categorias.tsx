9e69b09b2fff1774ecbacf8b64a65c3702f2f911
igoor <igorwallacy3@gmail.com>
2024-04-21 21:23:33 -0300

    sessao cetegorias

--- src/app/pages/categorias.tsx
+++ src/app/pages/categorias.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
+import { CiDesktopMouse1 } from 'react-icons/ci'
+import { FaHeadphones } from 'react-icons/fa'
 import { FaRegKeyboard } from 'react-icons/fa6'
+import { LuMonitorSmartphone } from 'react-icons/lu'
+import { PiComputerTowerDuotone } from 'react-icons/pi'
+import { TbDeviceIpadStar } from 'react-icons/tb'
 
 const Categorias = () => {
     return (
@@ -12,28 +17,28 @@ const Categorias = () => {
                             <h1>Teclados</h1>
                         </div>
                         <div className='flex items-center justify-center space-x-2 border-[1px] border-[#2A2A2A] p-1 px-3 rounded-lg w-full' >
-                            <FaRegKeyboard size={20} />
-                            <h1>Teclados</h1>
+                            <CiDesktopMouse1 size={20} />
+                            <h1>Mouses</h1>
                         </div>
                     </div>
                     <div className='flex md:max-w-[33%] w-full gap-4 justify-between p-1' >
                         <div className='flex items-center justify-center space-x-2 border-[1px] border-[#2A2A2A] p-1 px-3 rounded-lg w-full' >
-                            <FaRegKeyboard size={20} />
-                            <h1>Teclados</h1>
+                            <FaHeadphones  size={20} />
+                            <h1>Fones</h1>
                         </div>
                         <div className='flex items-center justify-center space-x-2 border-[1px] border-[#2A2A2A] p-1 px-3 rounded-lg w-full' >
-                            <FaRegKeyboard size={20} />
-                            <h1>Teclados</h1>
+                            <TbDeviceIpadStar  size={20} />
+                            <h1>Mousepads</h1>
                         </div>
                     </div>
                     <div className='flex md:max-w-[33%] w-full gap-4 justify-between p-1' >
                         <div className='flex items-center justify-center space-x-2 border-[1px] border-[#2A2A2A] p-1 px-3 rounded-lg w-full' >
-                            <FaRegKeyboard size={20} />
-                            <h1>Teclados</h1>
+                            <LuMonitorSmartphone  size={20} />
+                            <h1>Monitores</h1>
                         </div>
                         <div className='flex items-center justify-center space-x-2 border-[1px] border-[#2A2A2A] p-1 px-3 rounded-lg w-full' >
-                            <FaRegKeyboard size={20} />
-                            <h1>Teclados</h1>
+                            <PiComputerTowerDuotone   size={20} />
+                            <h1>Speakers</h1>
                         </div>
                     </div>
                 </div>
