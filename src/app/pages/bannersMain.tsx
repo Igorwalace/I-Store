2ea49d561bdba8e69b46bc3ff414f90de6b43131
igoor <igorwallacy3@gmail.com>
2024-04-22 11:22:01 -0300

    bannersMain pronta

--- src/app/pages/bannersMain.tsx
+++ src/app/pages/bannersMain.tsx
@@ -1,9 +1,57 @@
 import React from 'react'
+import Image from 'next/image'
 
 const BannersMain = () => {
-  return (
-    <div>BannerMain</div>
-  )
+    return (
+        <>
+            <main className='my-5 flex gap-6 justify-around items-center overflow-x-auto' >
+                <div className="md:min-w-[600px] md:min-h-[215px] h-[150px] w-full bg-[#36393C] rounded-xl flex items-center justify-center hover:scale-[1.01] duration-200 cursor-pointer">
+                    <div className="flex items-center justify-center md:space-x-9 space-x-3">
+
+                        <div className='text-white' >
+                            <h1 className='md:text-base text-sm font-light' >até
+                                <span className='md:text-[42px] text-4xl font-extrabold md:leading-[25px] text-[#8162FF]' >55%</span>
+                                <span className='md:text-base text-sm' >de</span>
+                            </h1>
+                            <h1 className='md:text-2xl text-xl font-extrabold md:leading-[10px] leading-3' >Desconto</h1>
+                            <h1 className='md:text-base text-sm font-light' >em Mouses</h1>
+                        </div>
+                        <div>
+                            <Image
+                                className='w-[113px] h-[97px] md:w-[140px] md:h-[120px]'
+                                src="/mouse_banner.png"
+                                width={500}
+                                height={500}
+                                alt="cart"
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div className="md:min-w-[600px] md:min-h-[215px] h-[150px] w-full bg-[#36393C] rounded-xl hidden md:flex items-center justify-center hover:scale-[1.01] duration-200 cursor-pointer">
+                    <div className="flex flex-row-reverse items-center justify-center md:space-x-9 space-x-3">
+
+                        <div className='text-white' >
+                            <h1 className='md:text-base text-sm font-light' >até
+                                <span className='md:text-[42px] text-4xl font-extrabold md:leading-[25px] text-[#8162FF]' >20%</span>
+                                <span className='md:text-base text-sm' >de</span>
+                            </h1>
+                            <h1 className='md:text-2xl text-xl font-extrabold md:leading-[10px] leading-3' >Desconto</h1>
+                            <h1 className='md:text-base text-sm font-light' >em Fones</h1>
+                        </div>
+                        <div>
+                            <Image
+                                className='w-[113px] h-[97px] md:w-[140px] md:h-[120px]'
+                                src="/phone_banner.png"
+                                width={500}
+                                height={500}
+                                alt="cart"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </>
+    )
 }
 
 export default BannersMain
