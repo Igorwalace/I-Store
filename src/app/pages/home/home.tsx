490d2edd87fef8867637dd38930693d8abba6878
igoor <igorwallacy3@gmail.com>
2024-04-22 21:35:48 -0300

    tinha erro sim

--- src/app/pages/home/home.tsx
+++ src/app/pages/home/home.tsx
@@ -1,29 +1,28 @@
 import React from 'react'
 
 //Imports
-import Header from './header'
-import Banner from './componentes/banner'
 import Categorias from './categorias'
 import Ofertas from './ofertas'
 import BannersMain from './bannersMain'
 import Teclados from './teclados'
-import BannerMobile from './componentes/bannerMobile'
 import BannerFretefree from './bannerMainFreteFree'
 import Mouses from './mouses'
-import Footer from './footer'
+import Header from '../header/header'
+import BannerMobile from './Componentes_Home/bannerMobile'
+import Footer from '../footer/footer'
 
 const Page = () => {
 
     return (
         <>
             <main className='px-5 md:px-10' ><Header /></main>
-                <Banner />
+                <BannersMain />
             <main className='px-5 md:px-10'>
                 <Categorias />
                 <Ofertas />
                 <BannersMain />
                 <Teclados />
-                <BannerMobile />
+                <BannerMobile/>
                 <BannerFretefree />
                 <Mouses />
             </main>
