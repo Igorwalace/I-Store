c38d94953362ea9373faeb54a2f86ce898a7b6b7
igoor <igorwallacy3@gmail.com>
2024-05-15 11:27:43 -0300

    link att

--- src/app/pages/footer/footer.tsx
+++ src/app/pages/footer/footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     return (
         <>
             <div className='bg-[#1A1A1A] p-2 text-[#676767] text-center overflow-hidden' >
-                <h1 className='hover:scale-105 duration-200' >Desenvolvido por <Link href='https://my-website-igor-eight.vercel.app/' target='_blank' className='text-[#8162FF] underline'>Igor Walace</Link></h1>
+                <h1 className='hover:scale-105 duration-200' >Desenvolvido por <Link href='https://meu-website-igor.vercel.app/' target='_blank' className='text-[#8162FF] underline'>Igor Walace</Link></h1>
             </div>
         </>
     )
